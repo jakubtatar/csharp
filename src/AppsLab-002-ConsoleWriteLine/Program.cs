285db3dc65b682f7a4c1921ffe4a264fd6b991cc
jakubtatar <jakub.tatar4@gmail.com>
2024-09-12 09:16:01 +0200

    Konzolu sme prikazy pridali:D

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+string greetings = "Ahoj svet!";
+Console.WriteLine(greetings);
+
+greetings = "Dovidenia svet!";
+Console.WriteLine(greetings);
+
+string name = "Jakub";
+Console.WriteLine("Hello " + name + "!");
+
