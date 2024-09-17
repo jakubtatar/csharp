1b22c0006968ed4bfa3b9a0da14f21070f57b0cc
jakubtatar <jakub.tatar4@gmail.com>
2024-09-17 08:12:16 +0200

    005 remake

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ *     **********
+ *     **********
+ *            ***
+ *     ***    ***
+ *            ***
+ *     ***    ***
+ *            ***
+ *     **********     
+ *     **********      
+*/
+
+int age = 15;
+string name = "Jakub";
+char firstLetter = 'J';
+bool isAdult = false; 
+float accountBalance = 0.26f;
+
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Name: " + name);
+Console.WriteLine("FIrst letter of name: " + firstLetter);
+Console.WriteLine("Is adult: " + isAdult);
+Console.WriteLine("Account ballance: " + accountBalance);
