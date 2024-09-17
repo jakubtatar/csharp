178bcceb3afeca26490bf883a12f37815367c972
jakubtatar <jakub.tatar4@gmail.com>
2024-09-17 09:08:54 +0200

    008 - urobene

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,16 @@
-﻿Console.WriteLine("Ako sa voláš?");
+﻿//Ako sa volas
+Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno);
 
-Console.WriteLine("Koľko máš rokov?");
+//Kolko mas rokov
+Console.WriteLine("\nKoľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
 Console.WriteLine("Máš " + vek + " rokov.");
+
+//Ake je tvoje oblubene jedlo
+Console.WriteLine("\nAké je tvoje obľúbené jedlo?");
+string? jedlo = Console.ReadLine();
+Console.WriteLine("Hmm, " + jedlo + ", to znie skvele!");
+
