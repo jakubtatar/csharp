b5293a860a256332656802b40eda6e01c1448857
jakubtatar <jakub.tatar4@gmail.com>
2024-09-17 08:35:33 +0200

    jj komenty pridane do 005

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,27 +1,19 @@
-﻿/*
- *     **********
- *     **********
- *            ***
- *     ***    ***
- *            ***
- *     ***    ***
- *            ***
- *     **********     
- *     **********      
-*/
-
+﻿
+//vytvaranie premennych
 int age = 15;
 string name = "Jakub";
 char firstLetter = 'J';
 bool isAdult = false; 
 float accountBalance = 0.26f;
 
+//vypisovanie premennych do konzloy
 Console.WriteLine("Age: " + age);
 Console.WriteLine("Name: " + name);
 Console.WriteLine("FIrst letter of name: " + firstLetter);
 Console.WriteLine("Is adult: " + isAdult);
 Console.WriteLine("Account ballance: " + accountBalance);
 
+//vypisanie smajlika do konzoly :]
 Console.WriteLine("");
 Console.WriteLine(" **********");
 Console.WriteLine(" **********");
