51e99d34e0cf234ac615213daa960a8b186d56be
jakubtatar <jakub.tatar4@gmail.com>
2024-09-24 09:23:44 +0200

    Arrays 015

--- src/AppsLab-015-Arrays/ArrayProcessor.cs
+++ src/AppsLab-015-Arrays/ArrayProcessor.cs
@@ -13,7 +13,14 @@ public class ArrayProcessor
     /// <returns>First element of the array, or 0 if array is empty.</returns>
     public int GetFirstElement(int[] numbers)
     {
-        throw new NotImplementedException();
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return numbers[0];
+        }
     }
 
     /// <summary>
@@ -24,7 +31,14 @@ public class ArrayProcessor
     /// <returns>Last element of the array, or 0 if array is empty.</returns>
     public int GetLastElement(int[] numbers)
     {
-        throw new NotImplementedException();
+        if (numbers.Length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return numbers[numbers.Length - 1];
+        }
     }
 
     /// <summary>
@@ -36,7 +50,14 @@ public class ArrayProcessor
     /// <returns>Element at the specified position, or 0 if position is out of range.</returns>
     public int GetElementAtPosition(int[] numbers, int position)
     {
-        throw new NotImplementedException();
+        if (numbers == null || position < 0 || position >= numbers.Length)
+        {
+            return 0;
+        }
+        else
+        {
+            return numbers[position];
+        }
     }
 
     /// <summary>
@@ -46,6 +67,6 @@ public class ArrayProcessor
     /// <returns>Length of the array.</returns>
     public int GetLength(int[] numbers)
     {
-        throw new NotImplementedException();
+        return numbers.Length;
     }
 }
