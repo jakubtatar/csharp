6e46ba459029d59af1aa7ccd3bcc5228fb013fcc
jakubtatar <jakub.tatar4@gmail.com>
2024-09-26 08:07:35 +0200

    Jednoducha Kalkulacka 013 pridana

---
+++ Jednoducha-Kalkulacka-013/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadajte znak pre matematicku operaciu:");
+            string operation = Console.ReadLine();
+            Console.WriteLine("Zadajte prve cislo:");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Zadajte druhe cislo:");
+            string number2 = Console.ReadLine();
+
+            double number1final = Double.Parse(number1);
+            double number2final = Double.Parse(number2);
+
+
+
+            if (operation == "+")
+            {
+                Console.WriteLine(number1final + number2final);
+            }
+            if (operation == "-")
+            {
+                Console.WriteLine(number1final - number2final);
+            }
+            if (operation == "*")
+            {
+                Console.WriteLine(number1final * number2final);
+            }
+            if (operation == "/")
+            {
+                Console.WriteLine(number1final / number2final);
+            }
+        }
+    }
+}
