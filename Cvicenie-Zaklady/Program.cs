07cf8cc6e8633c4ead67c614387d694ead12e733
jakubtatar <jakub.tatar4@gmail.com>
2024-10-08 08:36:25 +0200

    gotowo

--- Cvicenie-Zaklady/Program.cs
+++ Cvicenie-Zaklady/Program.cs
@@ -8,13 +8,26 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            int pocetSamohlasok = 0;
 
             Console.Write("Napiste text: ");
             string text = Console.ReadLine() ?? "0";
 
             int length = SpocitajPismena(text);
 
+            Char[] samohlasky = text.ToCharArray();                    
+
+            foreach(char c in samohlasky)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                    pocetSamohlasok++;
+                }
+            }
+
             Console.WriteLine("Text je dlhy " + text.Length + " znakov.");
+            Console.WriteLine("Text obsahuje " + pocetSamohlasok + " samohlasok.");
+
 
         }
 
@@ -23,6 +36,6 @@ namespace MyApp
             return text.Length;
         }
 
-        //DU: NECH SPOCITA SAMOHLASKY AJ, ROZISOVAT MALE A VELKE PISMENA> a,e,i,o,u / A,E,I,O,U;
+       //DU: NECH SPOCITA SAMOHLASKY AJ, ROZISOVAT MALE A VELKE PISMENA> a,e,i,o,u / A,E,I,O,U;
     }
 }
