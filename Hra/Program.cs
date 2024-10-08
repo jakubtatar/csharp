a831964203ad315db0f827c0839efddb1a5b2f0d
jakubtatar <jakub.tatar4@gmail.com>
2024-10-08 10:31:37 +0200

    iba pokusy

--- Hra/Program.cs
+++ Hra/Program.cs
@@ -6,30 +6,47 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+
             Random rnd = new Random();
             int vygenerovanecislo = rnd.Next(maxValue:100);
 
+            int pocetPokusov = 8;
+
             bool isGame = true;
+
 
             while (isGame == true)
             {
-                Console.WriteLine("Uhadni cislo:");
-                string hadanecislo = Console.ReadLine() ?? "0";
-                int hadanecisloint = int.Parse(hadanecislo);
+                if (pocetPokusov != 0)
+                {
+                    Console.WriteLine("Pokus: " + pocetPokusov);
+                    Console.WriteLine("Uhadni cislo:");
+                    string hadanecislo = Console.ReadLine() ?? "0";
+                    int hadanecisloint = int.Parse(hadanecislo);
 
-                if (hadanecisloint == vygenerovanecislo)
+                    if (hadanecisloint == vygenerovanecislo)
+                    {
+                        isGame = false;
+                        Console.WriteLine("Vyhrali ste!");
+                    }
+                    else if (hadanecisloint > vygenerovanecislo)
+                    {
+                        Console.WriteLine("Vase cislo je vacsie ako random cislo!");
+                        pocetPokusov--;
+                    }
+                    else if (hadanecisloint < vygenerovanecislo)
+                    {
+                        Console.WriteLine("Vase cislo je mensie ako random cislo!");
+                        pocetPokusov--;
+                    }
+                }
+                else
                 {
                     isGame = false;
-                    Console.WriteLine("Vyhrali ste!");
+                    Console.WriteLine("Dosli vam pokusy, prehrali ste!");
+                    
                 }
-                else if (hadanecisloint > vygenerovanecislo)
-                {
-                    Console.WriteLine("Vase cislo je vacsie ako random cislo!");
-                }
-                else if (hadanecisloint < vygenerovanecislo)
-                {
-                    Console.WriteLine("Vase cislo je mensie ako random cislo!");
-                }
+                
             }
 
         }
