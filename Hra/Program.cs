451f3f22643d5043d1a867ed2c83964d73bfbb48
jakubtatar <jakub.tatar4@gmail.com>
2024-10-08 10:20:04 +0200

    Hra

---
+++ Hra/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int vygenerovanecislo = rnd.Next(maxValue:100);
+
+            bool isGame = true;
+
+            while (isGame == true)
+            {
+                Console.WriteLine("Uhadni cislo:");
+                string hadanecislo = Console.ReadLine() ?? "0";
+                int hadanecisloint = int.Parse(hadanecislo);
+
+                if (hadanecisloint == vygenerovanecislo)
+                {
+                    isGame = false;
+                    Console.WriteLine("Vyhrali ste!");
+                }
+                else if (hadanecisloint > vygenerovanecislo)
+                {
+                    Console.WriteLine("Vase cislo je vacsie ako random cislo!");
+                }
+                else if (hadanecisloint < vygenerovanecislo)
+                {
+                    Console.WriteLine("Vase cislo je mensie ako random cislo!");
+                }
+            }
+
+        }
+    }
+}
