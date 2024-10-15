552615af817a7d61aeda64925f026eed08ee065b
jakubtatar <jakub.tatar4@gmail.com>
2024-10-15 10:49:18 +0200

    Hra Moja

---
+++ Cvicenie_OOP_grapocitacowa/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvicenie_OOP_grapocitacowa
+{
+    public class Player
+    {
+        public string ClassType { get; set; }
+        public int Power { get; set; }
+        public int HP { get; set; }
+        public int Mana { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        public Player(string classType, int power, int hP, int mana)
+        {
+            ClassType = classType;
+            Power = power;
+            HP = hP;
+            Mana = mana;
+        }
+        
+        public void DamagePlayer(Player player)
+        {
+            int HPofEnemy = player.HP;
+            int AttackofCurrentPlayer = this.Power;
+            int HPofEnemyAfterFight = HPofEnemy - AttackofCurrentPlayer;
+            player.HP = HPofEnemyAfterFight;
+        }
+    }
+}
---
+++ Cvicenie_OOP_grapocitacowa/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Cvicenie_OOP_grapocitacowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Bača Jano", 150, 200, 40000);
+            Player player2 = new Player("Zemepán Nikolaj Palfi", 10, 1950, 60);
+
+            while (player2.HP >= 0 && player1.HP >= 0)
+            {
+                Console.WriteLine("Player2 HP: " + player2.HP);
+                player1.DamagePlayer(player2);
+                Console.WriteLine("Player2 HP after Fight: " + player2.HP);
+                Thread.Sleep(0100);
+
+                Console.WriteLine("Player1 HP: " + player1.HP);
+                player2.DamagePlayer(player1);
+                Console.WriteLine("Player1 HP after Fight: " + player1.HP);
+                Thread.Sleep(0100);
+            }
+
+            if (player1.HP > player2.HP)
+            {
+                Console.WriteLine("Player1 is winner!");
+            }
+            else
+            {
+                Console.WriteLine("Player2 is winner!");
+            }
+            
+        }
+    }
+}
