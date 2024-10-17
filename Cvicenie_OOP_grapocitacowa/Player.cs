9278e07b5dced8fa94ede43c0b589956731c2138
jakubtatar <jakub.tatar4@gmail.com>
2024-10-17 08:47:54 +0200

    Gra Polska

--- Cvicenie_OOP_grapocitacowa/Player.cs
+++ Cvicenie_OOP_grapocitacowa/Player.cs
@@ -12,33 +12,73 @@ namespace Cvicenie_OOP_grapocitacowa
         public int Power { get; set; }
         public int HP { get; set; }
         public int Mana { get; set; }
+        public int CritChance { get; set; }
+        public int RefilManaChance { get; set; }
 
         public Player()
         {
 
         }
 
-        public Player(string classType, int power, int hP, int mana)
+        public Player(string classType, int power, int hP, int mana, int critChance, int refilManaChance)
         {
             ClassType = classType;
             Power = power;
             HP = hP;
             Mana = mana;
+            CritChance = critChance;
+            RefilManaChance = refilManaChance;
         }
         
         public void DamagePlayer(Player player)
         {
+            int damageMultiplier = 1;
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.CritChance)
+            {
+                damageMultiplier = 2;
+                Console.WriteLine(this.ClassType + " hit Critical!");
+            }
+            
+
             int HPofEnemy = player.HP;
-            int AttackofCurrentPlayer = this.Power;
+            int AttackofCurrentPlayer = this.Power * damageMultiplier;
             int HPofEnemyAfterFight = HPofEnemy - AttackofCurrentPlayer;
             player.HP = HPofEnemyAfterFight;
         }
 
-        public void HealPlayer()
+        public bool HealPlayer()
         {
-            int ManaOfPlayer = this.Mana;
-            int HPofPlayerAfterHeal = this.HP + ManaOfPlayer;
-            this.HP = HPofPlayerAfterHeal;
+            
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            if (randomNumber <= this.RefilManaChance)
+            {
+                this.Mana += 50;
+                Console.WriteLine(this.ClassType + " has refilled his Mana!");
+            }
+
+            if (this.Mana > 0)
+            {
+                int HpOfPlayer = this.HP;
+                int ManaOfPlayer = this.Mana;
+                int HPofPlayerAfterHeal = HpOfPlayer + ManaOfPlayer;
+                HpOfPlayer = HPofPlayerAfterHeal;
+                this.Mana = 0;
+                return true;
+            }
+            else
+            { 
+                return false; 
+            }
+           
+        }
+
+        public void RefilMana(int newMana)
+        {
+            this.Mana += newMana;
         }
     }
 }
--- Cvicenie_OOP_grapocitacowa/Program.cs
+++ Cvicenie_OOP_grapocitacowa/Program.cs
@@ -4,37 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player("Bača Jano", 150, 200, 60);
-            Player player2 = new Player("Zemepán Nikolaj Palfi", 10, 1950, 40);
+            //Hraci (Class, Power, HP, Mana, Kritical, RefilChanceMana)
+            Player player1 = new Player("Bača Jano", 10, 200, 60, 50, 5); 
+            Player player2 = new Player("Zemepán Nikolaj Palfi", 10, 250, 40, 10, 5);  
 
+            //Suboj medzi hracmi
             while (player2.HP >= 0 && player1.HP >= 0)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
+                //Player1 utoci
                 Console.WriteLine("Player2 HP: " + player2.HP);
                 player1.DamagePlayer(player2);
                 Console.WriteLine("Player2 HP after Fight: " + player2.HP);
-                player1.HealPlayer();
-                Console.WriteLine("Player2 HP after Heal: " + player2.HP);
+                if (player2.HP <= 20) //Player2 sa healuje
+                {
+                    bool isHealed = player2.HealPlayer();
+                    if (isHealed)
+                    {
+                        Console.WriteLine("Player2 was healed!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player2 doesn't have any mana left!");
+                    }
+                }
                 Thread.Sleep(0100);
 
-                Console.ForegroundColor = ConsoleColor.Red;
+                //Player2 utoci
                 Console.WriteLine("Player1 HP: " + player1.HP);
                 player2.DamagePlayer(player1);
                 Console.WriteLine("Player1 HP after Fight: " + player1.HP);
-                player2.HealPlayer();
-                Console.WriteLine("Player1 HP after Heal: " + player1.HP);
+                if (player1.HP <= 20) //Player1 sa healuje
+                {
+                    bool isHealed = player1.HealPlayer();
+                    if (isHealed)
+                    {
+                        Console.WriteLine("Player1 was healed!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player1 doesn't have any mana left!");
+                    }
+                }
                 Thread.Sleep(0100);
+
+              
+                
             }
 
+            //Vitaz bitky
             if (player1.HP > player2.HP)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Player1 is winner!");
+                Console.WriteLine($"Player1 {player1.ClassType} is winner!");
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Player2 is winner!");
+                Console.WriteLine($"Player2 {player2.ClassType} is winner!");
             }
             
         }
