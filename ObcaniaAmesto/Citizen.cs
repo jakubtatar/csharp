fec49fc529a9747b40aea915705659315d2bd174
jakubtatar <jakub.tatar4@gmail.com>
2024-10-22 10:41:25 +0200

    MEsto a OBCANIA DOMACCAAAAA ULOGAAAAAA

---
+++ ObcaniaAmesto/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObcaniaAmesto
+{
+    public class Citizen
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Citizen()
+        {
+
+        }
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void VypisInfo()
+        {           
+            Console.WriteLine("Meno: " + Name + ", Vek: " + Age);           
+        }
+    }
+}
---
+++ ObcaniaAmesto/City.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObcaniaAmesto
+{
+    public class City
+    {
+        public string NazovMiestecka { get; set; }
+        public List<Citizen> obcaniaList { get; set; }
+
+        public City()
+        {
+
+        }
+
+        public City(string nazovMiestecka, List<Citizen> obcaniaList)
+        {
+            NazovMiestecka = nazovMiestecka;
+            this.obcaniaList = obcaniaList;
+        }
+
+        public void PridajObcana(Citizen obcaniaList)
+        {
+            
+        }
+
+        public void VypisObcanov()
+        {
+            foreach (var citizen in obcaniaList)
+            {
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
