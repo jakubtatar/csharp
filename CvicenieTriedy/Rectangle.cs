b482a2d1ee47569dfd14324da6ed99071f4ca458
jakubtatar <jakub.tatar4@gmail.com>
2024-10-22 09:46:12 +0200

    Taky Rectangel a juraj janosik

---
+++ CvicenieTriedy/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvicenieTriedy
+{
+    internal class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+        
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Calculate()
+        {
+            int result = Width * Height;
+            return result;
+        }
+    }
+    
+    
+
+    
+
+}
